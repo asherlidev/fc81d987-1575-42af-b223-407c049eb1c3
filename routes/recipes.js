53e1a7ad7ba5cf1c5befa5e1f30dde5c0616f7d8
asherlidev <asherlidev95@gmail.com>
2022-03-13 07:33:28 +0000

    make recipte route

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,5 +1,22 @@
 var recipes = require('../recipes.json');
 var router = require('express').Router();
 
+router.get('/recipes', function(req, res) {
+    let page = req.query.page;
+    let limit = req.query.limit;
+
+    let result = '';
+
+    if (!page && !limit) {
+        result = recipes;    
+    }
+
+    if (page && limit) {
+        result = recipes.filter(recipe => recipe.id > (page - 1) * limit && recipe.id <= page * limit )
+    }
+
+    res.send(JSON.stringify(result));    
+})
+
 module.exports = router;
 
