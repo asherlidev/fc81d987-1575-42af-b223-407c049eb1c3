b27d38f3ad05ef2e76fcc97b549cff3f00027179
asherlidev <asherlidev95@gmail.com>
2022-03-13 07:42:35 +0000

    default value

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,19 +2,11 @@ var recipes = require('../recipes.json');
 var router = require('express').Router();
 
 router.get('/recipes', function(req, res) {
-    let page = req.query.page;
-    let limit = req.query.limit;
-
-    let result = '';
-
-    if (!page && !limit) {
-        result = recipes;    
-    }
-
-    if (page) {
-        limit = 3 || limit;
-        result = recipes.filter(recipe => recipe.id > (page - 1) * limit && recipe.id <= page * limit )
-    }
+    let page = 1 || req.query.page;
+    let limit = 3 || req.query.limit;   
+    
+    let result = recipes.filter(recipe => recipe.id > (page - 1) * limit && recipe.id <= page * limit )
+    
 
     res.status(200).send(JSON.stringify(result));    
 })
